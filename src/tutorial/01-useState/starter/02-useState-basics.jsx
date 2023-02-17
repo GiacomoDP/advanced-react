c45374a7fce42021cc2a165cd058483bccf189f0
Giacomo De Pol <giacomo.depol@hotmail.it>
2023-02-17 16:44:07 +0100

    useState basics

--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  //console.log(useState());
+  //const value = useState("hello")[0];
+  //console.log(value);
+
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <h4>you clicked {count} times</h4>
+      <button type="button" className="btn" onClick={handleClick}>
+        click me
+      </button>
+    </div>
+  );
 };
 
 export default UseStateBasics;
