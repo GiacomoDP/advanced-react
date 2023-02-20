b9863f244121193b73675c60306a6491f7811797
Giacomo De Pol <giacomo.depol@hotmail.it>
2023-02-20 18:46:26 +0100

    remove items

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -3,6 +3,15 @@ import React from "react";
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
 
+  const removeItem = (id) => {
+    console.log(id);
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  const clearAllItems = () => {
+    setPeople([]);
+  };
+
   return (
     <div>
       {people.map((person) => {
@@ -11,9 +20,20 @@ const UseStateArray = () => {
         return (
           <div key={id}>
             <h4>{name}</h4>
+            <button type="button" onClick={() => removeItem(id)}>
+              remove
+            </button>
           </div>
         );
       })}
+      <button
+        type="button"
+        style={{ margin: "2rem" }}
+        className="btn"
+        onClick={clearAllItems}
+      >
+        clear iteams
+      </button>
     </div>
   );
 };
