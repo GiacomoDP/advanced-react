4b5f553f5952cce9c63c992d388e383d1d0ede92
Giacomo De Pol <giacomo.depol@hotmail.it>
2023-02-20 19:27:46 +0100

    useState obj example

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 
 const UseStateObject = () => {
-  const [name, setName] = useState("peter");
-  const [age, setAge] = useState(45);
-  const [hobby, setHobby] = useState("tennis");
+  const [person, setPerson] = useState({
+    name: "Peter",
+    age: 45,
+    hobby: "tennis",
+  });
+
   const displayPerson = () => {
-    setName("john");
-    setAge(93);
-    setHobby("reading book");
+    setPerson({ name: "john", age: 93, hobby: "reading book" });
   };
 
   return (
     <>
-      <h3>{name}</h3>
-      <h3>{age}</h3>
-      <h4>Enjoys to :{hobby}</h4>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4>Enjoys to :{person.hobby}</h4>
       <button className="btn" onClick={displayPerson}>
         show john
       </button>
