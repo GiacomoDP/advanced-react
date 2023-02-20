dac0a7526e3e3f14600525f85698639fba810424
Giacomo De Pol <giacomo.depol@hotmail.it>
2023-02-20 19:59:03 +0100

    functional update form

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 const UseStateGotcha = () => {
   const [value, setValue] = useState(0);
   const handleClick = () => {
-    setValue(value + 1);
+    setValue((currentState) => {
+      const newState = currentState + 1;
+      return newState;
+    });
   };
   return (
     <div>
