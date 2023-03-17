73fb5b406a98799218e4e1af7e6993317e92854e
Giacomo De Pol <giacomo.depol@hotmail.it>
2023-03-17 13:06:10 +0100

    useEffect cleanup function

--- src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+  return (
+    <div>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        toggle component
+      </button>
+      {toggle && <RandomComponent />}
+    </div>
+  );
 };
 
+const RandomComponent = () => {
+  useEffect(() => {
+    console.log(" interestingggg");
+  }, []);
+  return <h1>hello there</h1>;
+};
 export default CleanupFunction;
